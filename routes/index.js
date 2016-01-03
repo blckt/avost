2e52cfe8f6bff6ae700736e65d3baafa82b6d4c3
Denis Sadilo <Denis Sadilo>
2016-01-03 23:41:25 +0300

    commit1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var rq=require('../workers/avostRequest');
+var fs=require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/pars',function(req,res){
+console.log(rq);
+    res.json(rq);
 
+});
 module.exports = router;
