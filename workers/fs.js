2eee7175b1cd232c2a3f5fd3608b9d8259cee0ab
blckt <blckt>
2016-01-07 19:03:54 +0200

    third

--- workers/fs.js
+++ workers/fs.js
@@ -2,20 +2,7 @@
 
 function init(app) {
     var request=require('request');
-var phantom=require('phantom');
 
-
-phantom.create(function (ph) {
-  ph.createPage(function (page) {
-    /* the page actions */
-  });
-}, {
-  dnodeOpts: {
-    weak: false
-  }
-});
-  //     console.log(app.locals.body);
-   return app; 
 }
 module.exports=function(app){
   
